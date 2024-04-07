a86e9bd504e0d8e19a3b2ec1af3004fffb986ab2
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2024-04-07 11:35:11 +0200

    initial commit for grrs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+
+/// Search for the pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for.
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file {}", args.path.display()))?;
+
+    for line in content.lines() {
+        println!("{line}");
+    }
+    Ok(())
+}
