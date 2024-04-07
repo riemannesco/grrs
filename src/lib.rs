02b762b5f9823524b0bdbb35ac7e185b7d32d574
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2024-04-07 18:02:27 +0200

    adding integration tests

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use anyhow::{Context, Result};
+use std::io::Write;
+
+pub fn find_matches<O: Write>(pattern: &str, content: &str, o: &mut O) -> Result<()> {
+    let mut is_match: bool = false;
+
+    if pattern.is_empty() {
+        return Err(std::fmt::Error).context("empty pattern");
+    }
+
+    for line in content.lines() {
+        if line.contains(pattern) {
+            is_match = true;
+            writeln!(o, "{}", line).context("could not write to specified output")?;
+        }
+    }
+
+    if is_match.eq(&false) {
+        writeln!(o, "{}", "no match found").context("counld not write to specified output")?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_a_match() {
+        let mut result = Vec::new();
+        let _ = find_matches("lorem", "lorem ipsum\ndolor sit amet", &mut result);
+        assert_eq!(result, b"lorem ipsum\n");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use std::io::{self, Write};
+use std::io;
 
 /// Search for the pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -11,35 +11,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches<O: Write>(pattern: &str, content: &str, o: &mut O) -> Result<()> {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(o, "{}", line).context("could not write to specified output")?;
-        }
-    }
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
     let args = Cli::parse();
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file {}", args.path.display()))?;
 
-    find_matches(&args.pattern, &content, &mut io::stdout())?;
+    grrs::find_matches(&args.pattern, &content, &mut io::stdout())?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn find_a_match() {
-        let mut result = Vec::new();
-        let _ = find_matches("lorem", "lorem ipsum\ndolor sit amet", &mut result);
-        assert_eq!(result, b"lorem ipsum\n");
-    }
 }
